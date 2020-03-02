e51321fc6d31df92b9c43c513934740b2e710968
Aluno <gaxavier593@gmail.com>
2020-03-02 09:28:09 -0300

    aula 3

---
+++ aula3.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <style media="screen">
+      .noticias h2{
+        color: #006600;
+        background-color: #b3ffb3;
+        font-size: 30px;
+
+      }
+      .noticias img{
+        width: ;
+        height: ;
+
+      }
+      body{
+        background-color:  #ffffe6;
+      }
+
+    </style>
+
+    <title></title>
+  </head>
+  <body>
+
+    <h2>Portal de noticia IFPR</h2>
+
+    <div class="noticias">
+      <h2>MATE ARGENTINO X MATE URUGUAIO</h2>
+      <p>A primeira característica importante da erva-mate argentina, assim como a erva-mate uruguaia,
+        é que a erva tem característica de repousada. Isso pois o processamento da planta até chegar ao consumidor final pode
+        levar até 12 meses para ser finalizado.</p>
+      <p>Dessa forma, a erva-mate dos países vizinhos tem coloração amarelada. Afinal, com o tempo a coloração verde da erva acaba sendo substituída por uma
+        coloração amarelada e um sabor mais amargo.</p>
+      <p>Mas esta é a grande similaridade entre a erva-mate argentina e a erva-mate uruguaia. Então, onde está a diferença entre estes dois estilos de mate
+         tão típicos de cada um dos países?</p>
+      <img src="https://www.baraoervamate.com.br/wp-content/uploads/2019/08/erva-mate-uruguaia.jpg" alt="Italian Trulli">
+    </div>
+
+    <div class="noticias">
+      <h2>MARCAS POR PAÍS</h2>
+      <table style="width:100%">
+    <tr>
+      <th>Name:</th>
+      <td>CANARIAS</td>
+    </tr>
+    <tr>
+      <th rowspan="2">Telephone:</th>
+      <td>55577854</td>
+    </tr>
+    <tr>
+      <td>55577855</td>
+    </tr>
+  </table>
+
+      </div>
+    <div class="noticias">
+      <h2>COMO FAZER MATE URUGUAIO</h2>
+
+      <img src="https://mateinbox.com.br/wp-content/uploads/2019/07/mate-uruguaio.jpg" alt="Italian Trulli">
+
+
+
+      </div>
+
+    </div>
+
+  </body>
+</html>

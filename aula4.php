d7f4ef3a811fe4c772dc3512de19d140b63313cf
Aluno <gaxavier593@gmail.com>
2020-03-09 09:30:41 -0300

    Aula 4

---
+++ aula4.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang=pt-br dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04 - CSS e HTML</title>
+
+      <link href="https://fonts.googleapis.com/css?family=Bellota|Roboto&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="aula4.css">
+  </head>
+  <body>
+
+    <div class="site">
+        <div class="topo">
+        <h3>  Topo do site</h3>
+        </div>
+        <div class="conteudo">
+            Corpo do site
+  </div>
+         <div class="rodape">
+            Desenvolvido por Gabriel Xavier
+
+          </div>
+
+          </div>
+
+        </div>
+    </div>
+
+  </body>
+</html>

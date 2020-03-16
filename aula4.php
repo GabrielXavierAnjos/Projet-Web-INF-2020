b499e1119b887c60af91d5ea2daf532ee80bcb92
Aluno <gaxavier593@gmail.com>
2020-03-16 09:43:37 -0300

    Aula botão

--- aula4.php
+++ aula4.php
@@ -12,15 +12,43 @@
 
     <div class="site">
         <div class="topo">
-        <h3>  Topo do site</h3>
+        <h3>  Criação de menu e botões </h3>
+
+        <div class="menu">
+          <ul>
+            <li><a href="#">Pagina Inicial</a></li>
+            <li><a href="#">Cadastro</a></li>
+            <li><a href="#">Contato</a></li>
+            <li><a href="#">Ajuda</a></li>
+            <li><a href="#">Login</a></li>
+
+          </ul>
+        </div>
+
         </div>
         <div class="conteudo">
-            Corpo do site
+            <h3>Criação de botões</h3>
+            <a href="#" class="botao botao-azul">Botão Azul</a>
+            <a href="#" class="botao botao-verde">Botão Verde</a>
+            <a href="#" class="botao botao-vermelho">Botão Vermelho</a>
+
+            <div class="noticias">
+              <h2>MATE ARGENTINO X MATE URUGUAIO</h2>
+              <p>A primeira característica importante da erva-mate argentina, assim como a erva-mate uruguaia,
+                é que a erva tem característica de repousada. Isso pois o processamento da planta até chegar ao consumidor final pode
+                levar até 12 meses para ser finalizado.</p>
+              <p>Dessa forma, a erva-mate dos países vizinhos tem coloração amarelada. Afinal, com o tempo a coloração verde da erva acaba sendo substituída por uma
+                coloração amarelada e um sabor mais amargo.</p>
+              <p>Mas esta é a grande similaridade entre a erva-mate argentina e a erva-mate uruguaia. Então, onde está a diferença entre estes dois estilos de mate
+                 tão típicos de cada um dos países?</p>
+              <img width="600" src="https://www.baraoervamate.com.br/wp-content/uploads/2019/08/erva-mate-uruguaia.jpg" alt="Italian Trulli">
+            </div>
   </div>
          <div class="rodape">
             Desenvolvido por Gabriel Xavier
 
           </div>
+
 
           </div>
 
